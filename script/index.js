d2e00ce6d9e5feb391a0216a599babb2b522d686
IsecEnglishClub <37940900+IsecEnglishClub@users.noreply.github.com>
2018-04-27 12:15:50 +0400

    Update index.js

--- script/index.js
+++ script/index.js
@@ -6,3 +6,138 @@ function toggleMenu(){
         ul.className = "mainNav-ul";
     }
 }
+
+function initMap() {
+    // Styles a map in night mode.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: {
+            lat: 40.192883,
+            lng: 44.508814
+        },
+        styles: [{
+            elementType: 'geometry',
+            stylers: [{
+                color: '#f5f5f5'
+            }]
+        }, {
+            elementType: 'labels.icon',
+            stylers: [{
+                visibility: 'off'
+            }]
+        }, {
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#616161'
+            }]
+        }, {
+            elementType: 'labels.text.stroke',
+            stylers: [{
+                color: '#f5f5f5'
+            }]
+        }, {
+            featureType: 'administrative.land_parcel',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#bdbdbd'
+            }]
+        }, {
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#eeeeee'
+            }]
+        }, {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#757575'
+            }]
+        }, {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#e5e5e5'
+            }]
+        }, {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#9e9e9e'
+            }]
+        }, {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#ffffff'
+            }]
+        }, {
+            featureType: 'road.arterial',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#757575'
+            }]
+        }, {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#dadada'
+            }]
+        }, {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#616161'
+            }]
+        }, {
+            featureType: 'road.local',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#9e9e9e'
+            }]
+        }, {
+            featureType: 'transit.line',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#e5e5e5'
+            }]
+        }, {
+            featureType: 'transit.station',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#eeeeee'
+            }]
+        }, {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#c9c9c9'
+            }]
+        }, {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#9e9e9e'
+            }]
+        }],
+    });
+    var marker;
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {
+            lat: 40.192883,
+            lng: 44.508814
+        }
+    });
+    marker.addListener('click', toggleBounce);
+
+    function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
+}
